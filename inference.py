6a3befbeb7578de236d9cf55c5d8d8959213a5f6
Eduardo Pooch <edupooch@gmail.com>
2021-04-19 12:09:44 -0300

    Adds gradcam++ output to the inference function

--- inference.py
+++ inference.py
@@ -6,8 +6,43 @@ from architecture.densenet121 import load_densenet121
 from PIL import Image
 import pydicom as dicom
 
+from interpretability.grad_cam import GradCAM, GradCamPlusPlus
+from skimage import io
+from torch import nn
+import numpy  as np
+import cv2
+
 MODEL_PATH = 'model/densenet_covid.pt'
-            
+# MODEL_PATH = 'model/densenet_covid_fulldata.pt'
+
+def gen_cam(image, mask):
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    heatmap = heatmap[..., ::-1]  # gbr to rgb
+
+    cam = heatmap + np.float32(image)
+    return norm_image(cam), (heatmap * 255).astype(np.uint8)
+
+def norm_image(image):
+    image = image.copy()
+    image -= np.max(np.min(image), 0)
+    image /= np.max(image)
+    image *= 255.
+    return np.uint8(image)
+
+def prepare_input(image):
+    image = image.copy()
+
+    means = np.array([0.485, 0.456, 0.406])
+    stds = np.array([0.229, 0.224, 0.225])
+    image -= means
+    image /= stds
+
+    image = np.ascontiguousarray(np.transpose(image, (2, 0, 1))) 
+    image = image[np.newaxis, ...] 
+
+    return torch.tensor(image, requires_grad=True)
+
 def main():
     parser = argparse.ArgumentParser(description='COVID Inference args')
     parser.add_argument('--dcm-path', type=str, required=True,
@@ -23,21 +58,37 @@ def main():
     model.load_state_dict(torch.load(MODEL_PATH))
     model.to(device)  
     model.eval()
+    net = model
     
     dcm_path = args.dcm_path           
     ds = dicom.dcmread(dcm_path, force=True)
     pixel_array = ds.pixel_array
     image = Image.fromarray(pixel_array) 
     image = image.convert('RGB')
-    image = val_transform(image)
-    data = image.unsqueeze(0)
+    data = val_transform(image)
+    data = data.unsqueeze(0)
     data = data.to(device)
+
+    # Grad-CAM++
+    img = np.array(image)
+    img = np.float32(cv2.resize(img, (224, 224))) / 255
+    inputs = data
+    inputs = prepare_input(img)
+    inputs = inputs.to(device)
+    layer_name = "features.denseblock4.denselayer16.conv2"
+    grad_cam_plus_plus = GradCamPlusPlus(net, layer_name)
+    mask_plus_plus = grad_cam_plus_plus(inputs, None)  # cam mask
+    cam, heatmap = gen_cam(img, mask_plus_plus)
+    grad_cam_plus_plus.remove_handlers()
+    # io.imsave('cam.jpg', cam)
     
     with torch.no_grad():
         output = model(data)
     score = f'{output[0,1].item():.6f}'
+    
     print(score)
-    return score
+    
+    return score, cam
 
 def get_transform():
     mean = [0.485, 0.456, 0.406]
